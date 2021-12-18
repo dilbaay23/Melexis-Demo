bdda041fb631d9330e38838f1129139054bea3e4
Moon <dilbaay@gmail.com>
2021-12-18 13:17:24 +0100

    Initial import

---
+++ src/main/java/com/melexis/reportapp/ReportappApplication.java
@@ -0,0 +1,13 @@
+package com.melexis.reportapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melexis/reportapp/ReportappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melexis.reportapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
